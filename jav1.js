e6caad9a325f40126f026b94f8d242ab93f8814a
anyigoh blessing <anyigohblessing@gmail.com>
2024-09-03 22:33:02 +0100

    html css exercise

---
+++ jav1.js
@@ -0,0 +1,7 @@
+var n=math.random();
+n=n*6;
+n=math.floor(n)+1;
+console.log(n);
+prompt("what is your name");
+prompt("what is thier name");
+var n =math.random(100);
---
+++ sub.py
@@ -0,0 +1,5 @@
+def get_data(): 
+ user_name = input("Enter your name: ") 
+ user_age = int(input("Enter your age: ")) 
+ data_tuple = (user_name, user_age) 
+ return data_tuple
